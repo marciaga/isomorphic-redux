ee8aeaa86b612167b523ce3836e004260dd9e540
marciaga <mark.arciaga@gmail.com>
2016-07-09 11:53:44 -0700

    Add circle yml

---
+++ circle_deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Test 123"
+# Deployment script for CircleCI to deploy or install an application.
+
+# This file is called automatically from circle.yml by CircleCI on a new build.
+# You should not run this script manually.
+
+# Example usage by CircleCI: ./circle_deploy.sh deploy production
+
+# Environment variables available to this script can be set at the following:
