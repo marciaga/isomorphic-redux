d9c01a47dff0bde6d784ba20c90bff9f2571be2a
marciaga <mark.arciaga@gmail.com>
2016-07-10 10:31:10 -0700

    Adding ansible to deploy script

--- circle_deploy.sh
+++ circle_deploy.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-echo "Test 123"
+
 # Deployment script for CircleCI to deploy or install an application.
-
 # This file is called automatically from circle.yml by CircleCI on a new build.
-# You should not run this script manually.
-
-# Example usage by CircleCI: ./circle_deploy.sh deploy production
-
+# This script should not be run manually.
 # Environment variables available to this script can be set at the following:
+export PYTHONPATH=/usr/local/lib/python2.7/dist-packages # add path for ec2.py
+
+# Copy over AWS credentials for boto library
+#sudo sh -c "echo '[Credentials]' >> /etc/boto.cfg"
+#sudo sh -c "echo 'aws_access_key_id = $AWS_ACCESS_KEY_ID' >> /etc/boto.cfg"
+#sudo sh -c "echo 'aws_secret_access_key = $AWS_SECRET_ACCESS_KEY' >> /etc/boto.cfg"
+
+set -e  # exit the script if any statement returns a non-true return value
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
+
+cd ansible
+
+
+# Exit with status code 1 if anything fails
+if [ "$?" == "1" ]; then
+	exit 1
+fi
