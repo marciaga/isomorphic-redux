5235ef7ab4c52bd40e4ee2a6575071ae7dd730bf
marciaga <mark.arciaga@gmail.com>
2016-07-10 10:34:58 -0700

    Add -y flag to ansible install

--- circle_deploy.sh
+++ circle_deploy.sh
@@ -15,7 +15,7 @@ set -e  # exit the script if any statement returns a non-true return value
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get -y install ansible
 
 cd ansible
 
